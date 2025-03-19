763587bbf02b924a7c7c16f7f93027f9502456d3
ksmama <markus.t.mattila@tuni.fi>
2025-03-19 18:33:10 +0200

    admin login endpoint

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { registerAdmin } from '../controllers/admin.js';
+import { registerAdmin, loginAdmin } from '../controllers/admin.js';
 
 const router = express.Router();
 
 // POST /admin/register
 router.post('/register', registerAdmin);
+
+// Adminin kirjautuminen
+router.post("/login", loginAdmin);
 
 export default router;
